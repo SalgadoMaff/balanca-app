dd41cb2fd02e9deb569ec64f2c939941e4317ad7
SalgadoMaff <lipesmaffini@gmail.com>
2021-06-06 11:18:07 -0400

    buscando refeições da api

--- src/api/meal.js
+++ src/api/meal.js
@@ -3,5 +3,8 @@ import api from "@/api/config"
 export default {
     create(userId, meal) {
         return api.post(`users/${userId}/meals`, meal)
+    },
+    findAllByUser(userId) {
+        return api.get(`users/${userId}/meals`)
     }
 }
--- src/store/meal.js
+++ src/store/meal.js
@@ -1,12 +1,17 @@
 import mealApi from "@/api/meal"
 
 const state = {
+    meals: []
 }
 
 const getters = {
+    getMeals: (state) => state.meals
 }
 
 const mutations = {
+    setMeals: (state, meals) => {
+        state.meals = meals
+    }
 }
 
 const actions = {
@@ -16,6 +21,13 @@ const actions = {
             return true
         }
         return false
+    },
+    findAllMealsByUser: async ({ commit }, userId) => {
+        commit("setMeals", [])
+        const response = await mealApi.findAllByUser(userId)
+        if (!(response instanceof Error)) {
+            commit("setMeals", response.data.meals)
+        }
     }
 }
 
