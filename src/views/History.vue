08c318ea4dd20c249122b3594e98c216fa0a1c4b
SalgadoMaff <lipesmaffini@gmail.com>
2021-06-06 11:18:50 -0400

    exibindo os dados da refeição no calendario

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,176 +1,48 @@
 <template>
-    <v-row class="fill-height">
-        <v-col>
-            <v-sheet height="64">
-                <v-toolbar flat>
-                    <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
-                        Hoje
-                    </v-btn>
-                    <v-btn fab text small color="grey darken-2" @click="prev">
-                        <v-icon small> mdi-chevron-left</v-icon>
-                    </v-btn>
-                    <v-btn fab text small color="grey darken-2" @click="next">
-                        <v-icon small> mdi-chevron-right</v-icon>
-                    </v-btn>
-                    <v-toolbar-title class="ml-2" v-if="$refs.calendar">
-                        {{ $refs.calendar.title }}
-                    </v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-menu bottom right>
-                        <template v-slot:activator="{ on, attrs }">
-                            <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
-                                <span>{{ typeToLabel[type] }}</span>
-                                <v-icon right> mdi-menu-down</v-icon>
-                            </v-btn>
-                        </template>
-                        <v-list>
-                            <v-list-item @click="type = 'day'">
-                                <v-list-item-title>Diário</v-list-item-title>
-                            </v-list-item>
-                            <v-list-item @click="type = 'week'">
-                                <v-list-item-title>Semanal</v-list-item-title>
-                            </v-list-item>
-                            <v-list-item @click="type = 'month'">
-                                <v-list-item-title>Mensal</v-list-item-title>
-                            </v-list-item>
-                        </v-list>
-                    </v-menu>
-                </v-toolbar>
-            </v-sheet>
-            <v-sheet height="600">
-                <v-calendar
-                    ref="calendar"
-                    v-model="focus"
-                    color="primary"
-                    :events="events"
-                    :event-color="getEventColor"
-                    :type="type"
-                    @click:event="showEvent"
-                    @click:more="viewDay"
-                    @click:date="viewDay"
-                    @change="updateRange"
-                ></v-calendar>
-                <v-menu
-                    v-model="selectedOpen"
-                    :close-on-content-click="false"
-                    :activator="selectedElement"
-                    offset-x
-                >
-                    <v-card color="grey lighten-4" min-width="350px" flat>
-                        <v-toolbar :color="selectedEvent.color" dark>
-                            <v-btn icon>
-                                <v-icon>mdi-pencil</v-icon>
-                            </v-btn>
-                            <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-                            <v-spacer></v-spacer>
-                            <v-btn icon>
-                                <v-icon>mdi-heart</v-icon>
-                            </v-btn>
-                            <v-btn icon>
-                                <v-icon>mdi-dots-vertical</v-icon>
-                            </v-btn>
-                        </v-toolbar>
-                        <v-card-text>
-                            <span v-html="selectedEvent.details"></span>
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-btn text color="secondary" @click="selectedOpen = false">
-                                Cancel
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-menu>
-            </v-sheet>
-        </v-col>
-    </v-row>
+    <calendar :events="meals" @change="findMeals"/>
 </template>
 
 <script>
+import Calendar from "@/components/Calendar"
+import {mapActions, mapGetters} from "vuex"
 export default {
+    name: "History",
+    components: {
+        Calendar
+    },
     data: () => ({
-        focus: "",
-        type: "month",
-        typeToLabel: {
-            day: "Diário",
-            month: "Mensal",
-            week: "Semanal"
-        },
-        selectedEvent: {},
-        selectedElement: null,
-        selectedOpen: false,
-        events: [],
-        colors: ["blue", "indigo", "deep-purple", "cyan", "green"],
-        names: [
-            "Meeting",
-            "Holiday",
-            "PTO",
-            "Travel",
-            "Event",
-            "Birthday",
-            "Conference",
-            "Party"
-        ]
+        meals: [],
     }),
-    mounted() {
-        this.$refs.calendar.checkChange()
+    computed: {
+        ...mapGetters("meal", ["getMeals"]),
+        ...mapGetters("user", ["getUser"])
     },
     methods: {
-        viewDay({ date }) {
-            this.focus = date
-            this.type = "day"
+        ...mapActions("meal", ["findAllMealsByUser"]),
+        async findMeals() {
+            await this.findAllMealsByUser(this.getUser._id)
+            this.createEvents()
         },
-        getEventColor(event) {
-            return event.color
-        },
-        setToday() {
-            this.focus = ""
-        },
-        prev() {
-            this.$refs.calendar.prev()
-        },
-        next() {
-            this.$refs.calendar.next()
-        },
-        showEvent({ nativeEvent, event }) {
-            const open = () => {
-                this.selectedEvent = event
-                this.selectedElement = nativeEvent.target
-                requestAnimationFrame(() =>
-                    requestAnimationFrame(() => (this.selectedOpen = true))
-                )
-            }
-            if (this.selectedOpen) {
-                this.selectedOpen = false
-                requestAnimationFrame(() => requestAnimationFrame(() => open()))
-            } else {
-                open()
-            }
-            nativeEvent.stopPropagation()
-        },
-        updateRange({ start, end }) {
-            const events = []
-            const min = new Date(`${start.date}T00:00:00`)
-            const max = new Date(`${end.date}T23:59:59`)
-            const days = (max.getTime() - min.getTime()) / 86400000
-            const eventCount = this.rnd(days, days + 20)
-            for (let i = 0; i < eventCount; i++) {
-                const allDay = this.rnd(0, 3) === 0
-                const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-                const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-                const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-                const second = new Date(first.getTime() + secondTimestamp)
-                events.push({
-                    name: this.names[this.rnd(0, this.names.length - 1)],
-                    start: first,
-                    end: second,
-                    color: this.colors[this.rnd(0, this.colors.length - 1)],
-                    timed: !allDay
+        createEvents() {
+            this.meals = []
+            this.getMeals.map(meal => {
+                this.meals.push({
+                    color: "cyan",
+                    start: this.formatDateTime(meal.createdAt),
+                    end: this.formatDateTime(meal.createdAt),
+                    timed: true,
+                    details: this.formatDetails(meal)
                 })
-            }
-            this.events = events
+            })
         },
-        rnd(a, b) {
-            return Math.floor((b - a + 1) * Math.random()) + a
+        formatDetails(meal) {
+            return meal.plate.reduce(this.formatDetailsText, "")
+        },
+        formatDetailsText(text, plate) {
+            if (text) {
+                return `${text}<br>${plate.foodId.name} - quantidade: ${plate.quantity}`
+            }
+            return `${plate.foodId.name} - quantidade: ${plate.quantity}`
         }
     }
 }
