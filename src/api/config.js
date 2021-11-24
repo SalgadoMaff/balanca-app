b93108859edae1ad9e5222f63fbe4158bf3633ed
SalgadoMaff <lipesmaffini@gmail.com>
2021-11-23 22:14:07 -0400

    arruma o erro quando expira sessão

--- src/api/config.js
+++ src/api/config.js
@@ -1,23 +1,25 @@
-import axios from 'axios';
-import store from '@/store';
+import axios from "axios"
+import store from "@/store"
 
 const axiosInstance = axios.create({ baseURL: process.env.VUE_APP_API_URL })
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  config.headers.common['Authorization'] = `Bearer ${token}`
-    return config
-});
+  const token = localStorage.getItem("token")
+  config.headers.common["Authorization"] = `Bearer ${token}`
+  return config
+})
 
 axiosInstance.interceptors.response.use(
   (config) => config,
   (error) => {
-    store.commit('error/setError', error.response.data)
-        if (error.response.status === 401) {
+    store.commit("error/setError", error.response.data)
+    if (error.response.status === 401) {
+      localStorage.removeItem("token")
+      store.dispatch("user/clearUserData")
       window.location.reload()
-        }
-    return error
     }
+    return error
+  }
 )
 
 export default axiosInstance
